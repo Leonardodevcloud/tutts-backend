1a5c66e3c59eb98c868e0daa4499d990966d9a43
Leonardo Santos santana <leonardosuporteflash@gmail.com>
2026-03-15 19:30:04 -0300

    Update index.js

--- src/modules/gerencial/index.js
+++ src/modules/gerencial/index.js
@@ -1,13 +1,11 @@
 /**
  * MÓDULO GERENCIAL - Análise Gerencial Semanal
- * Relatório semanal consolidado com KPIs, SLA por cliente,
- * ticket médio, variação de demanda e mínimo garantido.
- * Consome dados de bi_entregas + bi_garantido_cache.
  */
 const { createGerencialRouter } = require('./gerencial.routes');
+const { initGerencialTables } = require('./gerencial.migration');
 
 function initGerencialRoutes(pool, verificarToken) {
   return createGerencialRouter(pool, verificarToken);
 }
 
-module.exports = { initGerencialRoutes };
+module.exports = { initGerencialRoutes, initGerencialTables };
